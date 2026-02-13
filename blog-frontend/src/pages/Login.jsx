51c87ddbfa38d599945ac17438164c81223b890c
Abdulla Tamboli <153532401+Abdullla-Tamboli@users.noreply.github.com>
2026-02-13 19:52:36 +0530

    addd rendar

--- blog-frontend/src/pages/Login.jsx
+++ blog-frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://blog-application-vh8n.onrender.com/api/auth/login", {
         email,
         password,
       });
